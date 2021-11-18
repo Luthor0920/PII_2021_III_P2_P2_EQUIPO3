67f5293eb2452ecddae22b30403f317256ba94f2
Luthor0920 <franklin.gonzalez1@ujcv.edu.hn>
2021-11-18 16:18:15 -0600

    Modificacion Puesto, Cliente y Empleado

--- src/datos/clasesDatos/ClienteDatos.java
+++ src/datos/clasesDatos/ClienteDatos.java
@@ -12,15 +12,17 @@ public class ClienteDatos {
     public static String InsertarCliente(Cliente pCliente) throws SQLException {
         try{
             Connection cn = Conexion.obtenerConexion();
-            String sql = "INSERT INTO Cliente VALUES (?,?,?,?,?,?,?)";
+            String sql = "INSERT INTO Cliente VALUES (?,?,?,?,?,?,?,?,?)";
             PreparedStatement ps = cn.prepareStatement(sql);
             ps.setLong(1, pCliente.getCodigo());
-            ps.setString(2, pCliente.getTipoCliente());
-            ps.setString(3, pCliente.getOcupacion());
-            ps.setString(4, pCliente.getRecurrencia());
-            ps.setString(5, pCliente.getTallaCamisa());
-            ps.setInt(6, pCliente.getTallaPantalon());
-            ps.setString(7, pCliente.getReferencia());
+            ps.setLong(2, pCliente.getDNI());
+            ps.setString(3, pCliente.getNombre());
+            ps.setString(4, pCliente.getTipoCliente());
+            ps.setString(5, pCliente.getOcupacion());
+            ps.setString(6, pCliente.getRecurrencia());
+            ps.setString(7, pCliente.getTallaCamisa());
+            ps.setInt(8, pCliente.getTallaPantalon());
+            ps.setString(9, pCliente.getReferencia());
             ps.execute();
             ps.close();
             cn.close();
@@ -35,18 +37,20 @@ public class ClienteDatos {
         try{
             Connection cn = Conexion.obtenerConexion();
             Statement st = cn.createStatement();
-            String sql = "SELECT Codigo, TipoCliente, Ocupacion, Recurrencia, TallaCamisa, " +
+            String sql = "SELECT Codigo, DNI, Nombre, TipoCliente, Ocupacion, Recurrencia, TallaCamisa, " +
                     "TallaPantalon, Referencia FROM Cliente";
             ResultSet rs = st.executeQuery(sql);
             while(rs.next()){
                 Cliente cliente = new Cliente();
                 cliente.setCodigo(rs.getLong(1));
-                cliente.setTipoCliente(rs.getString(2));
-                cliente.setOcupacion(rs.getString(3));
-                cliente.setRecurrencia(rs.getString(4));
-                cliente.setTallaCamisa(rs.getString(5));
-                cliente.setTallaPantalon(rs.getInt(6));
-                cliente.setReferencia(rs.getString(7));
+                cliente.setDNI(rs.getLong(2));
+                cliente.setNombre(rs.getString(3));
+                cliente.setTipoCliente(rs.getString(4));
+                cliente.setOcupacion(rs.getString(5));
+                cliente.setRecurrencia(rs.getString(6));
+                cliente.setTallaCamisa(rs.getString(7));
+                cliente.setTallaPantalon(rs.getInt(8));
+                cliente.setReferencia(rs.getString(9));
                 listaCliente.add(cliente);
             }
             rs.close();
@@ -59,16 +63,18 @@ public class ClienteDatos {
     public static String ActualizarCliente(Cliente pCliente) throws SQLException {
         try{
             Connection cn = Conexion.obtenerConexion();
-            String sql = "UPDATE Cliente SET TipoCliente= ?, Ocupacion = ?, Recurrencia = ?, TallaCamisa = ?" +
+            String sql = "UPDATE Cliente SET DNI = ?, Nombre = ?, TipoCliente= ?, Ocupacion = ?, Recurrencia = ?, TallaCamisa = ?" +
                     "TallaPantalon = ?, Referencia = ? WHERE Codigo = ?";
             PreparedStatement ps = cn.prepareStatement(sql);
-            ps.setString(1, pCliente.getTipoCliente());
-            ps.setString(2, pCliente.getOcupacion());
-            ps.setString(3, pCliente.getRecurrencia());
-            ps.setString(4, pCliente.getTallaCamisa());
-            ps.setInt(5, pCliente.getTallaPantalon());
-            ps.setString(6, pCliente.getReferencia());
-            ps.setLong(7, pCliente.getCodigo());
+            ps.setLong(1, pCliente.getDNI());
+            ps.setString(2, pCliente.getNombre());
+            ps.setString(3, pCliente.getTipoCliente());
+            ps.setString(4, pCliente.getOcupacion());
+            ps.setString(5, pCliente.getRecurrencia());
+            ps.setString(6, pCliente.getTallaCamisa());
+            ps.setInt(7, pCliente.getTallaPantalon());
+            ps.setString(8, pCliente.getReferencia());
+            ps.setLong(9, pCliente.getCodigo());
             ps.execute();
             ps.close();
             cn.close();
@@ -92,5 +98,24 @@ public class ClienteDatos {
             return "Error "+e.getMessage();
         }
         return null;
+    }
+    public static List<Cliente> BuscarCliente() throws SQLException{
+        List<Cliente> listaCliente = new ArrayList<>();
+        try{
+            Connection cn = Conexion.obtenerConexion();
+            Statement st = cn.createStatement();
+            String sql = "";
+            ResultSet rs = st.executeQuery(sql);
+            if (rs.next()){
+                do{
+                    Cliente cliente = new Cliente();
+                }while(rs.next());
+            }else{
+                throw new SQLException("No se encontro coincidencia");
+            }
+        }catch (SQLException e){
+            throw new SQLException(e.getMessage());
+        }
+        return listaCliente;
     }
 }
